a1c644ba8d6ee56b0ccbf76c9830bd4d0103f9c3
fredfortier <fredfortier@gmail.com>
2014-06-24 20:44:00 -0400

    Minor changes following blog updates

--- src/main/webapp/WebContent/sample-plugin/CustomAddContentDialog.js
+++ src/main/webapp/WebContent/sample-plugin/CustomAddContentDialog.js
@@ -1,9 +1,10 @@
-define([ "dojo/_base/declare", "ecm/widgets/dialog/AddContentItemDialog" ],
-    function (declare, AddContentItemDialog) {
+define([ "dojo/_base/declare", "dojo/dom-construct", "ecm/widgets/dialog/AddContentItemDialog" ],
+    function (declare, domConstruct, AddContentItemDialog) {
         return declare('CustomAddContentItemDialog', [AddContentItemDialog], {
             postCreate: function () {
                 this.inherited(arguments);
                 console.log('overriding the postCreate function');
+                domConstruct.place('<h1>Hello World!</h1>', this.domNode, 'first');
             }
         });
     });
